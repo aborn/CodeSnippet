97084e85af2ae9583ed29b94a5e92102ff60b566
Guobao Jiang <loveaborn@foxmail.com>
2014-01-22 16:41:12 +0800

    add fun in kit.el

--- elisp/kit.el
+++ elisp/kit.el
@@ -16,3 +16,15 @@
 (message "hello word")
 (message "input argument are %s" argv)
 (message "first argument is %s" (elt argv 0))
+(message "system-type:%s system-name:%s" system-type system-name)
+                                        ;(message (string-to-int))
+
+(defun cal-salary (iday)
+  "Calculate the salary for intern in shanghai"
+  (interactive "p")
+  (setq value (+ 800 (* 0.8 (- (* iday 180)800))))
+  (setq tvalue (- (* iday 180) value))
+  (message "You work %d day, and salary is %d, tax is %d."  iday value tvalue)
+  )
+
+(cal-salary (string-to-number (elt argv 0)))
