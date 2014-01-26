09ebb499abc339acd92bee79f55316dc8167d233
aborn <loveaborn@foxmail.com>
2014-01-26 10:55:35 +0800

    add utf8-to-gbk.sh

---
+++ utf8-to-gbk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# conv utf-8(linux) encoding file to gbk (windows)
+
+filenames=$(ls *.m)
+
+for file in ${filenames}
+do
+    echo ${file}
+    iconv -f UTF-8 -t GBK ${file} -o "${file}.out" >>/dev/null
+    if [ $? -eq 0 ];then
+        mv "${file}.out" ${file}
+    else
+        echo "conv failed in file ${file}" 
+        exit -1
+    fi
+done
+
+echo "conv all files to encoding gbk success."
