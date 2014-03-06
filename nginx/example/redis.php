6e042015abdfbbf25395685ced9da75c13938dce
Guobao Jiang <loveaborn@foxmail.com>
2014-03-06 17:03:05 +0800

    complete nginx/example

---
+++ nginx/example/redis.php
@@ -0,0 +1,17 @@
+<html>
+     <head>
+        <meta charset="utf-8">
+        <title>[redis]性能测试页面</title>
+        <link href="/bs/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/bs/dist/css/starter-template.css" rel="stylesheet">
+     </head>
+     <body>
+
+    <?php include('navbar.html'); ?>
+
+    <?php
+        require_once('redis_action.php');
+       ?>
+
+     </body>
+     </html>
