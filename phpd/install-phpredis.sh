4a4dfbc6d127e886ca0ad2900cf69050bd45b015
Guobao Jiang <loveaborn@foxmail.com>
2014-03-06 20:40:35 +0800

    update

--- phpd/install-phpredis.sh
+++ phpd/install-phpredis.sh
@@ -10,3 +10,7 @@ cd src
 ./configure --with-php-config=/home/vagrant/php5/bin/php-config
 make
 make install
+
+## then  add two lines in php5/etc/php.in
+# extension=redis.so
+# extension_dir = "/home/aborn/php5/lib/php/extensions/no-debug-non-zts-20121212"
