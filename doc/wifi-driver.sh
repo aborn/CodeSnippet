ec25b710fe0c7aa3a2aaa440d720534a8c0f8e83
Guobao Jiang <loveaborn@foxmail.com>
2014-03-10 16:03:22 +0800

    add wifi-driver.sh

---
+++ doc/wifi-driver.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+###################################################
+# install wifi-driver in linux
+# OURLINK WU708E
+# realtek 8192du
+###################################################
+
+# download driver source code
+# git clone https://github.com/lwfinger/rtl8192du.git
+git clone https://github.com/aborn/rtl8192du.git
+
+# compile
+cd DIR
+sudo -s
+make
+sudo make install
+
+
+########################################################
+
+# QA: implicit declaration of function ‘daemonize’
+http://askubuntu.com/questions/290452/installing-workaround-realtek-drivers-for-edimax-but-getting-error
+
+add
+void daemonize(char *, char *);
+in include/osdep_service.h 
+
+
+## process 
+aborn@aborn-lenovo ~/sw/drivers/rtl8192du
+ % sudo make install
+install -p -m 644 8192du.ko  /lib/modules/3.8.0-35-generic/kernel/drivers/net/wireless/
+mkdir -p /lib/firmware/rtlwifi
+cp -n rtl8192dufw*.bin /lib/firmware/rtlwifi/.
+/sbin/depmod -a 3.8.0-35-generic
+
+## process
+aborn@aborn-lenovo ~/VirtualBox/share/wifi_network_driver/Driver/linux/driver/rtl8192DU_linux_v4.0.0_5260.20120921
+ % sudo make install
+install -p -m 644 8192du.ko  /lib/modules/3.8.0-35-generic/kernel/drivers/net/wireless/
+/sbin/depmod -a 3.8.0-35-generic
+
+
+
