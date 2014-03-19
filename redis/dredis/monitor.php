be8fa71580b848cbbe60b32c695848c41a2f1f4a
Guobao Jiang <loveaborn@foxmail.com>
2014-03-19 16:54:48 +0800

    rename dmanager.php to manager.php

--- redis/dredis/monitor.php
+++ redis/dredis/monitor.php
@@ -1,4 +1,19 @@
 <?php
+  /***
+   * INTRO
+   *   This class uses to monitor all redis cluster pool
+   *
+   * DEPENDS
+   *  phpredis and iredis
+   *
+   * VERSION
+   *  2014-03-19 1.0
+   *
+   * AUTHOR
+   *  Aborn Jiang
+   *
+   ***/
+
 
 require_once '/vagrant_data/data/rediska/library/Rediska.php';
 // Initialize Rediska
