9c7b03d3987dd1feec58cd8d8d01515f93809ad0
aborn <loveaborn@foxmail.com>
2014-04-19 21:21:34 +0800

    add conkeror install scripts

---
+++ conkeror/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###################################################
+# ./install.sh install conkeror to your sys
+# 2014-04-19
+###################################################
+cp conkeror /usr/local/bin/
+cp conkeror.desktop /usr/share/applications
+cp contrib/art/conkeror.png /usr/share/icons/hicolor/scalable/apps/
+ 
