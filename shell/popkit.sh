72b574fe8a7193d861916508653f5b9d61259576
guobao.jiang <loveaborn@foxmail.com>
2015-10-11 15:30:58 +0800

    add popkit.sh

---
+++ shell/popkit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "-----------------------"
+echo "start exectue:"
+echo `date`
+cd ~/github/popkit/
+echo `pwd`
+git pull
+echo `date`
+echo "finished!"
+echo "-----------------------"
+
