e0ca35bb72b2b75174f20c062acd4faebb7895d9
guobao.jiang <loveaborn@foxmail.com>
2015-12-12 22:42:32 +0800

    update locale

---
+++ shell/updatelocale.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# warning: the VM is running with native name encoding of latin1 which may cause Elixir to malfunction as it expects utf8.
+# Please ensure your locale is set to UTF-8 (which can be verified by running "locale" in your shell)
+sudo update-locale LC_ALL=en_US.UTF-8
