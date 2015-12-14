7dfc79c6aa91c2c43b13e543f080006530274f13
蒋国宝 <loveaborn@foxmail.com>
2015-12-14 23:36:56 +0800

    add elixir install

--- mac/vim-pathogen.sh
+++ mac/vim-pathogen.sh
@@ -2,3 +2,4 @@
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle 
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/elixir-lang/vim-elixir.git ~/.vim/bundle/vim-elixir
