dc7b9f1e2b526fcda439f49e8a48b10464d07352
guobao.jiang <loveaborn@foxmail.com>
2015-12-18 17:03:30 +0800

    jquery常用操作

---
+++ jQuery/jQueryFreq.js
@@ -0,0 +1,11 @@
+/**
+ * jQuery常用的一些操作
+ */
+
+// 从当前myId找最近为table的parent
+$('#myId').closest('table');
+
+// 是否选中 jQuery 1.6
+$('#checkMeOut').prop('checked'); // true
+
+$('#checkBox').prop('checked', true); // 设置为true
