8368fe1e31bacd76c2444ffc655641d5eeb8eee5
guobao.jiang <loveaborn@foxmail.com>
2016-03-15 14:44:02 +0800

    update

--- sql/appkit.sql
+++ sql/appkit.sql
@@ -3,3 +3,7 @@ update `Ele_Config` set `status` = 0 where eleScene = 'appconfig' and parentId=0
 
 # 删除
 delete from Ele_Scene where id=38
+
+# 删除某个场景的数据
+delete from `Ele_Content_Config` where eleId in (select eleId from Ele_Config where `eleScene` = 'testop2');
+delete from Ele_Config where `eleScene` = 'testop2';
