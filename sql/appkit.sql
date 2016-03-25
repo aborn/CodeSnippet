a83006df9e0562a6fa0bccb1ee6d758e4434565b
guobao.jiang <loveaborn@foxmail.com>
2016-03-25 14:41:23 +0800

    按月统计

--- sql/appkit.sql
+++ sql/appkit.sql
@@ -7,3 +7,6 @@ delete from Ele_Scene where id=38
 # 删除某个场景的数据
 delete from `Ele_Content_Config` where eleId in (select eleId from Ele_Config where `eleScene` = 'testop2');
 delete from Ele_Config where `eleScene` = 'testop2';
+
+# 按月统计上线量
+select month(`createTime`), count(*) from `Ele_Config_Package` where year(createTime) = 2015 group by month(createTime)
