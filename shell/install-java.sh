cff4a2a95d9e01c678df8981d2c1cb83db252d43
蒋国宝 <loveaborn@foxmail.com>
2016-04-17 15:27:22 +0800

    install java

---
+++ shell/install-java.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# unbuntu加入java源
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+
+# 安装java-7
+sudo apt-get install oracle-java7-installer
+
+# 安装java-8
+sudo apt-get install oracle-java8-installer 
