99dab5937efaf02e8c82df761a59e5044be1ed7c
guobao.jiang <loveaborn@foxmail.com>
2016-05-25 19:06:54 +0800

    update sql

---
+++ sql/add_column.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `BInfo` ADD `buId` INT(11) DEFAULT '0' COMMENT ' 事业群id';
+ALTER TABLE `BInfo` ADD `appUnionId` VARCHAR(2048) DEFAULT '0' NOT NULL COMMENT ' 公司范围内appid，唯一标识';
+ALTER TABLE `BInfo` ADD `extInfo` TEXT DEFAULT '' COMMENT '扩展信息';
