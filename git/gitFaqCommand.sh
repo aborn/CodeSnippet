58b17bf509a3a34311e874538832b9893a2f1bab
Aborn Jiang <loveaborn@foxmail.com>
2016-06-10 09:36:28 +0800

    update gitfqa

--- git/gitFaqCommand.sh
+++ git/gitFaqCommand.sh
@@ -2,3 +2,7 @@
 git reset --hard <commit-hash>;
 ## 远程也进行强制同步
 git push -f origin <branch>
+## 设置用户名
+git config user.name "Aborn Jiang"
+git config --global user.name "Aborn Jiang"
+git config --list
