b98b5902cef70ee8b329b0d9944034dba1b76a18
guobao.jiang <loveaborn@foxmail.com>
2016-06-12 11:09:11 +0800

    update

--- git/gitFaqCommand.sh
+++ git/gitFaqCommand.sh
@@ -6,3 +6,16 @@ git push -f origin <branch>
 git config user.name "Aborn Jiang"
 git config --global user.name "Aborn Jiang"
 git config --list
+## 初始化
+git init
+git add .
+git commit -m "Initial commit"
+## 与远端关联
+git remote add origin <remote-git-url>
+# example: git remote add origin git@code.dianpingoa.com:mobile/knb-ua-kit.git
+git push --set-upstream origin master
+# 如果远端有了，则git pull下
+git pull origin master
+
+## 撤销add
+git reset <filename>
