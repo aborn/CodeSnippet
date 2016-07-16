96dd55ffcc3e0146d4f05f9cca3383d1407e97e6
Aborn Jiang <loveaborn@foxmail.com>
2016-07-16 12:42:15 +0800

    install port

--- mac/macFreqSW.sh
+++ mac/macFreqSW.sh
@@ -5,3 +5,8 @@ sudo easy_install pip
 
 # install tree
 brew install tree
+
+# install port
+wget https://distfiles.macports.org/MacPorts/MacPorts-2.3.4-10.11-ElCapitan.pkg
+# add PATH
+# export PATH=$PATH:/opt/local/bin
