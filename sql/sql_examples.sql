51cda1b524d08aa033fb947cb2c6ac9901d6fae1
guobao.jiang <loveaborn@foxmail.com>
2016-09-09 15:01:27 +0800

    update@2016-09-09 15:01:27

--- sql/sql_examples.sql
+++ sql/sql_examples.sql
@@ -18,3 +18,5 @@ insert into temp values("gr", 108333, "goooogle");
 #insert into tablename values(a1,a2), (b1,b2);
 #insert into account values(17, 17.2);
 
+# 查询表中某个字段(eleName)是否重复行
+select * from <table-name> where a="avalue" group by `eleName` having count(eleName)<>1
