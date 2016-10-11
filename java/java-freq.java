cdc0e352073878f497306b2640f7b0f21a20b137
guobao.jiang <loveaborn@foxmail.com>
2016-10-11 17:25:06 +0800

    commit by magit in emacs@2016-10-11 17:25:05

--- java/java-freq.java
+++ java/java-freq.java
@@ -1,4 +1,5 @@
 
 public static void main(String[] args){
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    //return "redirect:/index.html?c=scene&key=hot";
 }
