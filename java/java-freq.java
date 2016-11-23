85e869bbf2bbdd9f98a1d16383518ee43f1baf87
guobao.jiang <loveaborn@foxmail.com>
2016-11-23 20:39:58 +0800

    commit by magit in emacs@2016-11-23 20:39:57

--- java/java-freq.java
+++ java/java-freq.java
@@ -2,4 +2,6 @@
 public static void main(String[] args){
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     //return "redirect:/index.html?c=scene&key=hot";
+    for (Map.Entry<String, EleDataAtom> entry : eleDataAtomMap.entrySet()) {
+    }
 }
