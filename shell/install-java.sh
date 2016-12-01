5bba72105c3b810bfd0132e42b6a844705f9aa5f
guobao.jiang <loveaborn@foxmail.com>
2016-12-01 18:11:22 +0800

    force switch java version

--- shell/install-java.sh
+++ shell/install-java.sh
@@ -17,6 +17,9 @@ sudo apt-get install oracle-java7-installer
 # 安装java-8
 sudo apt-get install oracle-java8-installer
 
+## 强制切换到java-7
+sudo update-java-alternatives -s java-7-oracle
+
 # add JAVA_HOME and JAVA_BIN env in .bshrc or .zshrc
 # export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 # export JAVA_BIN=$JAVA_HOME/bin
