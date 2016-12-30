fdd8482cccff03229fd5388c3e83bea6eb8ae793
guobao.jiang <loveaborn@foxmail.com>
2016-12-30 17:17:33 +0800

    commit by magit in emacs@2016-12-30 17:17:32

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -3,9 +3,10 @@
 # https://help.ubuntu.com/community/MysqlPasswordReset
 
 
-# 停止mysql服务
+# mysql服务操作
 sudo /etc/init.d/mysql stop
 sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql restart   #重启
 
 # ubuntu中删除mysql -- remove mysql from ubuntu
 sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-5.6 mysql-client-core-5.6 mysql-server-core-5.7 mysql-client-core-5.7
@@ -24,6 +25,7 @@ sudo apt-get install mysql-server mysql-client
 # 查看数据引擎
 show engines;
 
+# 设置远程可访问mysql
 # 注意: /etc/mysql/mysql.conf.d/mysqld.cnf
 # bind-address           = 127.0.0.1
 
