f7cd7170e335d0d112db274fcb485647ee8d9ee0
aborn <loveaborn@foxmail.com>
2017-04-23 22:46:50 +0800

    add backup command

--- intellij/intellij.sh
+++ intellij/intellij.sh
@@ -4,6 +4,7 @@
 
 # for linux the key-map file is in directory
 ~/.IntelliJIdea<xx>/config/keymaps
+cp ~/.IntelliJIdea2017.1/config/keymaps/IdeaEmacsLinux.xml ~/github/popkit/intellij/
 
 # for macOs, key-map file is in directory
 ~/Library/Preferences/IntelliJIdea<xx>/keymaps/
