e788f6ed64bfbb2537de064e3d2d848153a0fd6e
guobao.jiang <guobao.jiang@dianping.com>
2018-01-18 21:13:47 +0800

    commit by magit in emacs@2018-01-18 21:13:47

--- js/semantic-freq.js
+++ js/semantic-freq.js
@@ -2,3 +2,10 @@
 
 // 下拉列表设置值,设置值为2
 $('#eleType').dropdown("set selected", 2);
+
+// dropdown的change事件
+$('.valueType-dropdown').on('change', function () {
+    var value = $(this).dropdown('get item').attr('value');
+    $('#valueType').val(value);
+    console.log($('#valueType').val());
+});
