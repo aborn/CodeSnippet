11eda2aa441a6f9d560a6d0348fea3c1f6bff46d
guobao.jiang <guobao.jiang@dianping.com>
2018-02-09 15:29:32 +0800

    commit by magit in emacs@2018-02-09 15:29:31

--- js/semantic-freq.js
+++ js/semantic-freq.js
@@ -7,6 +7,7 @@ $('#eleType').dropdown("set selected", 2);
 $('.valueType-dropdown').on('change', function () {
     var value = $(this).dropdown('get item').attr('value');
     //var value = $(this).val();   // 如果是option
+    // var value = $(this).find('option[selected]').attr('data-url');  // option
     $('#valueType').val(value);
     console.log($('#valueType').val());
 });
