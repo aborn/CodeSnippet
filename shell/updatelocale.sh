fb3f1ad37fbf9f555122877d1629d619d0f87e9b
Aborn Jiang <loveaborn@foxmail.com>
2018-09-09 08:50:39 +0800

    add abornme.conf

--- shell/updatelocale.sh
+++ shell/updatelocale.sh
@@ -2,4 +2,5 @@
 
 # warning: the VM is running with native name encoding of latin1 which may cause Elixir to malfunction as it expects utf8.
 # Please ensure your locale is set to UTF-8 (which can be verified by running "locale" in your shell)
+sudo dpkg-reconfigure locales
 sudo update-locale LC_ALL=en_US.UTF-8
