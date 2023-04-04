619fba7d01309a382cc124742d0fc8998df46d42
aborn <loveaborn@foxmail.com>
2023-04-04 16:16:39 +0800

    add docker command

--- shell/demo.sh
+++ shell/demo.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+CURRENT_DIR=`pwd`
+
+echo "script dir $SCRIPT_DIR"
+echo "dir $DIR"
+echo "CURRENT_DIR $CURRENT_DIR"
+cd $DIR
+echo "CURRENT_DIR `pwd`"
 echo "parameter $#"
 
 echo "$1"
