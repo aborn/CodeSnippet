cecee4d1d9bd4cc09359359f0041f27a21e4e65c
aborn <loveaborn@foxmail.com>
2025-03-24 09:44:11 +0800

    commit by magit in emacs@2025-03-24 09:44:11

--- shell/delBranch.sh
+++ shell/delBranch.sh
@@ -3,7 +3,6 @@
 # 删除分支
 echo "脚本名称: $0"
 
-
 # 检查是否传入了参数
 if [ $# -eq 0 ]; then
     echo "错误：未传入要删除的分支名。用法: $0 <分支名>"
@@ -15,7 +14,7 @@ echo "----------------"
 git branch | grep -q "$1"
 if [ $? -eq 0 ]; then
     echo "本地分支 $1 存在，删除它"
-    git branch --delete $1
+    git branch --delete feature/$1
 else
     echo "本地分支 $1 不存在，无需处理"
 fi
